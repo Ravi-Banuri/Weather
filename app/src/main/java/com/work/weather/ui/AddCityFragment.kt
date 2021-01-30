272d94614c5ec9c5ef64969899ddfc4526d303ba
Ravi <banuri.ravi@gmail.com>
2021-01-30 18:43:20 +0530

    Update AddCityFragment.kt
    SHowing current location in Map

--- app/src/main/java/com/work/weather/ui/AddCityFragment.kt
+++ app/src/main/java/com/work/weather/ui/AddCityFragment.kt
@@ -5,19 +5,22 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.OnMyLocationChangeListener
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
 import com.work.weather.R
@@ -29,6 +32,11 @@ import java.util.*
 
 
 class AddCityFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mLocationRequest: LocationRequest
+    var mLastLocation: Location? = null
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mFusedLocationClient: FusedLocationProviderClient? = null
 
     private lateinit var mMap: GoogleMap
     private  var cityName:String? = null
@@ -43,12 +51,16 @@ class AddCityFragment : Fragment(), OnMapReadyCallback {
         return inflater.inflate(R.layout.fragment_add_city, container, false)
     }
 
+    override fun onPause() {
+        super.onPause()
+        mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         map.onCreate(savedInstanceState)
         map.onResume()
         map.getMapAsync(this)
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         btn_save_city.setOnClickListener {
             saveCity()
         }
@@ -57,6 +69,10 @@ class AddCityFragment : Fragment(), OnMapReadyCallback {
     @SuppressLint("UseRequireInsteadOfGet")
     override fun onMapReady(gMap: GoogleMap) {
         mMap = gMap
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 120000 // two minute interval
+        mLocationRequest.fastestInterval = 120000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
         checkPermissionsAndShowLocation()
         mMap.setOnMapClickListener {
             var markerOptions= MarkerOptions()
@@ -97,13 +113,19 @@ class AddCityFragment : Fragment(), OnMapReadyCallback {
     }
 
     fun checkPermissionsAndShowLocation() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ){
+                requestPermissions( arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+            } else {
 
-        if(ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED ){
-            requestPermissions( arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+                mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+                mMap!!.isMyLocationEnabled = true
+            }
         } else {
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
             mMap!!.isMyLocationEnabled = true
         }
     }
@@ -118,6 +140,27 @@ class AddCityFragment : Fragment(), OnMapReadyCallback {
             checkPermissionsAndShowLocation()
         }
     }
+
+
+    internal var mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                //The last location in the list is the newest
+                val location = locationList.last()
+                mLastLocation = location
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker?.remove()
+                }
+
+                val latLng = LatLng(location.latitude, location.longitude)
+
+                //move map camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11.0F))
+            }
+        }
+    }
+
 
 
 }
